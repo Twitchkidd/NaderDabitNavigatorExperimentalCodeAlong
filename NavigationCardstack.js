ef22f6bc00745d390dba02f0a8d9e50e346ffd3e
Twitchkidd <field.gareth@gmail.com>
2017-01-27 12:54:02 -0500

    Push and pop routes

--- NavigationCardstack.js
+++ NavigationCardstack.js
@@ -7,27 +7,69 @@ const {
 
 let styles = {};
 
-const Home = () => (
+const Home = ({ navigate }) => (
   <View style={styles.container}>
-    <Text>Hello from @Home</Text>
+    <Text>Hello from @Home!</Text>
+    <Text onPress={() => navigate("push", { key: "About" })}>
+      Go to About
+    </Text>
   </View>
 )
 
-function reducer(state) {
+const About = ({ navigate }) => (
+  <View style={styles.container}>
+    <Text>Hello from @About!</Text>
+    <Text onPress={() => navigate("pop")}>
+      Go back to Home
+    </Text>
+  </View>
+)
+
+function reducer(state, action, route) {
   if (!state) {
     return {
       index: 0,
       routes: [{key: "Home"}]
     };
   }
+  switch(action) {
+    case "push": {
+      const routes = state.routes.slice();
+      routes.push(route);
+      return {
+        ...state,
+        index: routes.length - 1,
+        routes
+      }
+    }
+    case "pop": {
+      if (state.index <= 0) return state;
+      const routes = state.routes.slice(0, -1);
+      return {
+        ...state,
+        index: routes.length - 1,
+        routes
+      }
+    }
+    default:
+      return state;
+  }
 }
 
 class App extends Component {
   state = { navState: reducer() }
+  _navigate = (action, route) => {
+    const navState = reducer(this.state.navState, action, route);
+    this.setState({
+      navState
+    });
+  }
   _renderScene = (props) => {
     switch(props.scene.route.key) {
       case "Home":
-        return <Home />
+        return <Home navigate={this._navigate} />
+      case "About":
+        return <About navigate={this._navigate} />
     }
   }
   render() {
